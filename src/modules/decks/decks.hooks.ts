156b1665c83d26d6a06f811fbb29dd3dfa0359b1
alieissa <aeissa.o@gmail.com>
2024-01-29 21:45:12 -0500

    6 - Relocate deck from logic Moved deck form state control logic to hooks and created a reducer that wraps the useReducer.
    This code will most likely be moved back
    to a component level once the NewDeck and
    Deck components are refactored to use one
    common component.6 - Relocate deck from logic
    Moved deck form state control logic to
    hooks and created a reducer that wraps
    the useReducer.
    
    This code will most likely be moved back
    to a component level once the NewDeck and
    Deck components are refactored to use one
    common component.6 - Relocate deck from logic
    Moved deck form state control logic to
    hooks and created a reducer that wraps
    the useReducer.
    
    This code will most likely be moved back
    to a component level once the NewDeck and
    Deck components are refactored to use one
    common component.6 - Relocate deck from logic
    Moved deck form state control logic to
    hooks and created a reducer that wraps
    the useReducer.
    
    This code will most likely be moved back
    to a component level once the NewDeck and
    Deck components are refactored to use one
    common component.6 - Relocate deck from logic
    Moved deck form state control logic to
    hooks and created a reducer that wraps
    the useReducer.
    
    This code will most likely be moved back
    to a component level once the NewDeck and
    Deck components are refactored to use one
    common component.6 - Relocate deck from logic
    Moved deck form state control logic to
    hooks and created a reducer that wraps
    the useReducer.
    
    This code will most likely be moved back
    to a component level once the NewDeck and
    Deck components are refactored to use one
    common component.

--- src/modules/decks/decks.hooks.ts
+++ src/modules/decks/decks.hooks.ts
@@ -1,8 +1,10 @@
 /** @format */
 
-import { useEffect } from 'react'
+import _ from 'lodash'
+import { useEffect, useReducer } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import * as api from 'src/api'
+import { Questions } from '../questions'
 import * as Select from './decks.selectors'
 import { NDecks } from './decks.types'
 export function useDecks() {
@@ -10,7 +12,7 @@ export function useDecks() {
   const decks = useSelector(Select.decks)
   const dispatch = useDispatch()
   const getDecks = api.request({ method: 'GET', url: 'decks' })
-  
+
   useEffect(() => {
     dispatch({
       type: 'GetDecks',
@@ -24,31 +26,154 @@ export function useDecks() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  // TODO return [status, decks]
   return { status, decks }
 }
 
-export function useCreateDeck():[NDecks.State['createStatus'], (deck: any) => void] {
+export function useCreateDeck(): [
+  NDecks.State['createStatus'],
+  (deck: any) => void,
+] {
   const dispatch = useDispatch()
-  const status = useSelector(
-    Select.createStatus,
-  )
+  const status = useSelector(Select.createStatus)
   const postDeck = api.request({
     url: 'decks',
     method: 'POST',
   })
 
+  // TODO Update deck type
   const createDeck = (deck: any) => {
     dispatch({
-      type: 'CreateDeck'
+      type: 'CreateDeck',
     })
 
     postDeck(deck).then((result) => {
       dispatch({
         type: 'DeckCreated',
-        result
+        result,
       })
     })
   }
 
   return [status, createDeck]
+}
+
+export function useDeckById(
+  id: NDecks.Deck['id'],
+): [NDecks.State['getStatus'][string], NDecks.Deck, (deck: any) => void] {
+  const deck = useSelector(Select.deckById(id))
+  const status = useSelector(Select.deckByIdStatus(id))
+  const dispatch = useDispatch()
+  const getDeck = api.request({ method: 'GET', url: `decks/${id}` })
+  const putDeck = api.request({ method: 'PUT', url: `decks/${id}` })
+
+  useEffect(() => {
+    if (deck) {
+      return
+    }
+
+    dispatch({
+      type: 'GetDeck',
+      id,
+    })
+    getDeck().then((result: any) => {
+      dispatch({ type: 'DeckLoaded', result, id })
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const updateDeck = (deck: any) => {
+    dispatch({
+      type: 'UpdateDeck',
+    })
+
+    putDeck(deck).then((result) => {
+      dispatch({
+        type: 'DeckUpdated',
+        result,
+      })
+    })
+  }
+
+  return [status, deck, updateDeck]
+}
+
+// All Form related logic will eventually be moved
+// to a component that will be used to display both
+// DeckPage and NewDeck pages.
+export type DeckFormState = Omit<NDecks.Initial, 'questions'> & {
+  questions: _.Dictionary<Questions.Initial>
+}
+export type DeckFormAction =
+  | {
+      type: 'ADD_QUESTION'
+    }
+  | {
+      type: 'DELETE_QUESTION'
+      question: Questions.Initial
+    }
+  | {
+      type: 'UPDATE_QUESTION'
+      question: Questions.Initial
+    }
+  | {
+      type: 'UPDATE_DECK'
+      name: NDecks.Initial['name']
+      description: NDecks.Initial['description']
+    }
+  | {
+      type: 'SET_DECK'
+      deck: NDecks.Deck
+    }
+
+const getInitState = () => {
+  const deck = NDecks.Initial({})
+  return {
+    ...deck,
+    questions: _.keyBy(deck.questions, '__key__'),
+  }
+}
+
+function reducer(state: DeckFormState, action: DeckFormAction) {
+  switch (action.type) {
+    case 'ADD_QUESTION': {
+      const newQuestion = Questions.Initial({})
+      return {
+        ...state,
+        questions: {
+          ...state.questions,
+          [newQuestion.__key__]: newQuestion,
+        },
+      }
+    }
+    case 'DELETE_QUESTION': {
+      return {
+        ...state,
+        questions: _.omit(state.questions, action.question.__key__),
+      }
+    }
+    case 'UPDATE_QUESTION': {
+      return {
+        ...state,
+        questions: _.set(
+          state.questions,
+          action.question.__key__,
+          action.question,
+        ),
+      }
+    }
+    case 'UPDATE_DECK': {
+      return {
+        ...state,
+        name: _.trim(action.name),
+        description: _.trim(action.description),
+      }
+    }
+    
+    default:
+      return state
+  }
+}
+export function useDeckFormReducer() {
+  return useReducer(reducer, getInitState())
 }
