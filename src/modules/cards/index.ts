6643134e960d803e9f1886b714b655443609cbed
alieissa <aeissa.o@gmail.com>
2024-05-06 18:01:02 -0400

    #43 Remove unused card component.

--- src/modules/cards/index.ts
+++ src/modules/cards/index.ts
@@ -1,6 +1,6 @@
 /** @format */
 
-export { default as Card } from './Card'
+
 export { default as CardForm } from './CardForm'
 export { default as TestCard } from './TestCard'
 export * from './cards.hooks'
