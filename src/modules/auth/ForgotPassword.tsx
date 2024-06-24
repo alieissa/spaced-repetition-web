29139b7061897c4b2a9c9652dfcbc566d49af68d
alieissa <aeissa.o@gmail.com>
2024-06-24 10:51:23 -0400

    86 Finish up forgot password ui.

--- src/modules/auth/ForgotPassword.tsx
+++ src/modules/auth/ForgotPassword.tsx
@@ -1,7 +1,7 @@
 import { FormikState, useFormik } from 'formik'
 import { ChangeEvent } from 'react'
-import { Form, Message } from 'semantic-ui-react'
-import { SPButton, SPInput, SPText } from 'src/components'
+import { Container, Form, Message } from 'semantic-ui-react'
+import { SPButton, SPHeader, SPInput, SPText } from 'src/components'
 import { styles } from 'src/styles'
 import { async } from 'src/utils'
 import * as Yup from 'yup'
@@ -23,23 +23,32 @@ function ForgotPasswordForm(props: Props) {
   return (
     <Form
       data-testid="forgot-password-form"
+      className="flex-column bordered p-1r"
       onSubmit={props.onNotifyForgotPassword}
     >
-      <SPInput
-        data-testid="forgot-password-form-email-input"
-        type="email"
-        value={props.form.values.email}
-        onChange={props.onChangeEmail}
-      />
-      {emailError && (
-        <SPText
-          data-testid="forgot-password-form-email-error"
-          style={styles['mt-0.5r']}
-        >
-          {props.form.errors.email}
-        </SPText>
-      )}
-      <SPButton data-testid="forgot-password-form-btn" type="submit">
+      <Form.Field error={emailError}>
+        <label htmlFor="email">Email</label>
+        <SPInput
+          data-testid="forgot-password-form-email-input"
+          type="email"
+          value={props.form.values.email}
+          onChange={props.onChangeEmail}
+        />
+        {emailError && (
+          <SPText
+            data-testid="forgot-password-form-email-error"
+            style={styles['mt-0.5r']}
+          >
+            {props.form.errors.email}
+          </SPText>
+        )}
+      </Form.Field>
+      <SPButton
+        data-testid="forgot-password-form-btn"
+        className="align-self-end"
+        type="submit"
+        disabled={emailError}
+      >
         Send
       </SPButton>
     </Form>
@@ -70,27 +79,31 @@ export default function ForgotPassword() {
     onChangeEmail: handleChangeEmail,
     onNotifyForgotPassword: handleNotifyForgotPassword,
   }
-  return async.match(notifyForgotPasswordStatus)({
-    Untriggered: () => <ForgotPasswordForm {...commonProps} />,
-    Loading: () => <ForgotPasswordForm {...commonProps} />,
-    Failure: () => (
-      <>
-        <div data-testid="forgot-password-form-error">
-          <Message negative>
-            <Message.Header>Error attempting to send email</Message.Header>
-            <p>Please try again.</p>
-          </Message>
-        </div>
-        <ForgotPasswordForm {...commonProps} />
-      </>
-    ),
-    Success: () => (
-      <div data-testid="forgot-password-form-success">
-        <Message positive>
-          <Message.Header>Email sent</Message.Header>
-          <p>An email with a reset link has been sent to you.</p>
-        </Message>
+  return (
+    <Container>
+      <div className="justify-space-between bordered p-1r mb-1r">
+        <SPHeader as="h1">Spaced Reps</SPHeader>
       </div>
-    ),
-  })
+
+      {async.match(notifyForgotPasswordStatus)({
+        Untriggered: () => <ForgotPasswordForm {...commonProps} />,
+        Loading: () => <ForgotPasswordForm {...commonProps} />,
+        Failure: () => (
+          <>
+            <div data-testid="forgot-password-form-error">
+              <Message negative>Unable to send email</Message>
+            </div>
+            <ForgotPasswordForm {...commonProps} />
+          </>
+        ),
+        Success: () => (
+          <div data-testid="forgot-password-form-success">
+            <Message positive>
+              An email with a reset link has been sent to you.
+            </Message>
+          </div>
+        ),
+      })}
+    </Container>
+  )
 }
