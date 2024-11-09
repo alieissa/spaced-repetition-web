275c8c26b36cf9060d2b694ff2d78f8475737416
alieissa <aeissa.o@gmail.com>
2024-11-09 15:50:08 -0500

    Remove redux packages and store

--- src/store.ts
+++
@@ -1,8 +0,0 @@
-/** @format */
-
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
-
-const reducers = combineReducers({
-})
-
-export default configureStore({ reducer: reducers })
